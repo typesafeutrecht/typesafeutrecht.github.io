b970d34f7afcc0c5cbec2e5468c14b672759d856
Mike Kotsur <miccots@gmail.com>
2019-04-14 10:38:17 +0200

    Prettier

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
-
-
 export default () => (
-    <React.Fragment>
-        <Header />
-        <Main />
-        <Footer />
-    </React.Fragment>
-)
+  <React.Fragment>
+    <Header />
+    <Main />
+    <Footer />
+  </React.Fragment>
+);
