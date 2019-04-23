83c75876b54249be6cf12d29e708664dd4338172
Mike Kotsur <miccots@gmail.com>
2019-04-23 20:48:38 +0200

    Updates

---
+++ precache-manifest.aef797bc7cb9a38ec19c28d7cc5028cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "75821592d7abec131386",
+    "url": "/static/css/main.f9759d72.chunk.css"
+  },
+  {
+    "revision": "75821592d7abec131386",
+    "url": "/static/js/main.e44d4cf3.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c44b846c37ea496b6fcd",
+    "url": "/static/js/2.34b4fde6.chunk.js"
+  },
+  {
+    "revision": "3ca731e838837c5f1df8a2c674972f08",
+    "url": "/static/media/logo.3ca731e8.png"
+  },
+  {
+    "revision": "a504f8568070c2e2a905e15abce5a354",
+    "url": "/index.html"
+  }
+];
