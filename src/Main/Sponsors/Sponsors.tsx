32bd18f190c345c784f345640c0b8c753c8ada81
Mike Kotsur <miccots@gmail.com>
2019-04-24 13:21:19 +0200

    Added Elsevier as a sponsor

--- src/Main/Sponsors/Sponsors.tsx
+++ src/Main/Sponsors/Sponsors.tsx
@@ -4,8 +4,9 @@ import './Sponsors.css';
 export default () => (<div className="pre-footer">
     <b>SPONSORS</b>
     <div className="sponsors">
-        <img src="/images/sponsors/mindspace.png"/>
-        <img src="/images/sponsors/bol.png"/>
+        <img src="/images/sponsors/elsevier.png"/>
         <img src="/images/sponsors/bdr.png"/>
+        <img src="/images/sponsors/bol.png"/>
+        <img src="/images/sponsors/mindspace.png"/>
     </div>
 </div>)
